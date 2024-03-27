e1cc155e84429903b6d4df09860f25aef16b7039
Daniel Duberg <danielduberg@gmail.com>
2024-03-27 11:03:59 +0100

    Added sleep

--- lib/utility/tests/timing_test.cpp
+++ lib/utility/tests/timing_test.cpp
@@ -4,14 +4,20 @@
 // Catch2
 #include <catch2/catch_test_macros.hpp>
 
+// STL
+#include <thread>
+
 TEST_CASE("Timing")
 {
+	using namespace std::chrono_literals;
+
 	ufo::Timing t("Test");
 
 	t[1].start("What what");
 	t[1][2].start("OMG");
 	t[1][3].start("Wiee");
 	t[1][3][1].start("Hum");
+	std::this_thread::sleep_for(5ms);
 	t[1][3][1].stop();
 	t[1][3].stop();
 	t[5][-3].start("Hi");
