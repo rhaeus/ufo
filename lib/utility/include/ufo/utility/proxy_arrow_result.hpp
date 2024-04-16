53955816d9e3432c59ddd2c4ddf9a989ecc6515b
Daniel Duberg <danielduberg@gmail.com>
2024-04-16 11:26:59 +0200

    Added proxy_arrow_result

---
+++ lib/utility/include/ufo/utility/proxy_arrow_result.hpp
@@ -0,0 +1,68 @@
+/*!
+ * UFOMap: An Efficient Probabilistic 3D Mapping Framework That Embraces the Unknown
+ *
+ * @author Daniel Duberg (dduberg@kth.se)
+ * @see https://github.com/UnknownFreeOccupied/ufomap
+ * @version 1.0
+ * @date 2022-05-13
+ *
+ * @copyright Copyright (c) 2022, Daniel Duberg, KTH Royal Institute of Technology
+ *
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, Daniel Duberg, KTH Royal Institute of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef UFO_UTILITY_PROXY_ARROW_RESULT_HPP
+#define UFO_UTILITY_PROXY_ARROW_RESULT_HPP
+
+// STL
+#include <utility>
+
+namespace ufo
+{
+template <class T>
+struct proxy_arrow_result {
+	proxy_arrow_result(T const& value) noexcept(noexcept(T(value))) : value_(value) {}
+
+	proxy_arrow_result(T&& value) noexcept(noexcept(T(std::move(value))))
+	    : value_(std::move(value))
+	{
+	}
+
+	constexpr T* operator->() noexcept { return &value_; }
+
+	constexpr T const* operator->() const noexcept { return &value_; }
+
+ private:
+	T value_;
+};
+}  // namespace ufo
+
+#endif  // UFO_UTILITY_PROXY_ARROW_RESULT_HPP
