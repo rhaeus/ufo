60e2954e79c18c719876f40090f8edacdc59e26d
Ramona Häuselmann <ramona.haeuselmann@gmail.com>
2024-11-15 12:51:52 +0100

    Added condition to fetchContent to avoid duplicates

--- lib/plan/1stparty/map/map.cmake
+++ lib/plan/1stparty/map/map.cmake
@@ -1,10 +1,13 @@
-Include(FetchContent)
+find_package(ufomap QUIET)
+if (NOT ufomap_FOUND)
+  Include(FetchContent)
 
-FetchContent_Declare(
-  ufomap
-  GIT_REPOSITORY https://github.com/UnknownFreeOccupied/ufomap.git
-  GIT_TAG        v2
-  GIT_PROGRESS   TRUE
-)
+  FetchContent_Declare(
+    ufomap
+    GIT_REPOSITORY https://github.com/UnknownFreeOccupied/ufomap.git
+    GIT_TAG        v2
+    GIT_PROGRESS   TRUE
+  )
 
-FetchContent_MakeAvailable(ufomap)
+  FetchContent_MakeAvailable(ufomap)
+endif()
